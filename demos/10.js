f239c8b7521b9e45e42926f5decf158247b56b0d
Easyli <easyli@users.noreply.github.com>
2017-08-17 15:12:55 +0800

    Update 10.js

--- demos/10.js
+++ demos/10.js
@@ -4,7 +4,7 @@ const app = new Koa();
 
 const main = async function (ctx, next) {
   ctx.response.type = 'html';
-  ctx.response.body = await fs.readFile('./demos/template.html', 'utf8');
+  ctx.response.body = await fs.readFile('./template.html', 'utf8');
 };
 
 app.use(main);
